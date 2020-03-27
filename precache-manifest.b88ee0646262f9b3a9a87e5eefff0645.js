0c11bfabf18ffa5d3f75bf9e2cb0f654d1e1b1a9
sahin88 <sahinmuratogur@gmail.com>
2020-03-27 18:54:28 +0100

    Updates

---
+++ precache-manifest.b88ee0646262f9b3a9a87e5eefff0645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f665eb0baa5ac3fdd6f331a07209738",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "0683f44b4e6541af2ae3",
+    "url": "/coronavirus/static/css/main.abdbbdce.chunk.css"
+  },
+  {
+    "revision": "7ba525fa8164af1ccc47",
+    "url": "/coronavirus/static/js/2.f300117d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronavirus/static/js/2.f300117d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0683f44b4e6541af2ae3",
+    "url": "/coronavirus/static/js/main.4922a4dd.chunk.js"
+  },
+  {
+    "revision": "ccb2d93b06846eedfd16",
+    "url": "/coronavirus/static/js/runtime-main.0f820ea0.js"
+  }
+]);
